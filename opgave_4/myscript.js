9b8a947a565d50dacda7e4d7ca76d16cd9913ad5
MartinLaursen <111317@rts-365.dk>
2018-01-10 10:07:21 +0100

    Opgave 4
    opgave 4 er blevet lavet færdig, den mangler kommentar

--- opgave_4/myscript.js
+++ opgave_4/myscript.js
@@ -1,10 +1,42 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    let span;
-    let textNode;
-
-    let div = document.querySelectorAll('div');
-    span = document.createElement('span');
-    span.appendChild(document.createTextNode('*'));
-    div.appendChild(span);
-    document.div.appendChild(div);
+    
+    document.querySelector('.button').addEventListener('click', () =>{
+        const form = document.querySelector('#kontakt');
+        kontakt(form);
+    })
 });
+let span;
+let textNode;
+    function kontakt (form) {
+       let vaildSpan = document.querySelectorAll('.valid');
+       vaildSpan.forEach(function(span) {
+           span.remove();           
+       });
+
+    if (form.name.value == 0) {
+    let navn = document.getElementById('navn');
+    span = document.createElement('span');
+    span.className = 'valid';
+    span.appendChild(document.createTextNode('*'));
+    navn.appendChild(span);
+    event.preventDefault();
+    }
+
+    if (form.mail.value == 0) {
+        let email = document.getElementById('email');
+        span = document.createElement('span');
+        span.className = 'valid';
+        span.appendChild(document.createTextNode('*'));
+        email.appendChild(span);
+        event.preventDefault();
+    }
+    
+    if (form.msg.value == 0) {
+        let msg = document.getElementById('msg');
+        span = document.createElement('span');
+        span.className = 'valid';
+        span.appendChild(document.createTextNode('*'));
+        msg.appendChild(span);
+        event.preventDefault();
+    }
+}
